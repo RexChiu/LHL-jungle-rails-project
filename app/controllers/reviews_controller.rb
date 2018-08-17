a9721f372bc468fdbdda6b8635f3a743bdb15844
Rex Chiu <rex.chiu@outlook.com>
2018-08-17 14:06:52 -0400

    added filter in reviews_controller to protect against anonymous reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,21 @@
 class ReviewsController < ApplicationController
-  def create
-    puts "review_params: #{review_params}"
-    @review = Review.new(review_params)
 
-    if @review.save
-      redirect_to product_url(params[:product_id]), notice: 'review created!'
-    else
-      redirect_to product_url(params[:product_id]), notice: 'some failure'
+  before_filter do
+    if current_user.blank?
+      redirect_to product_url(params[:product_id])
+    end
+  end
+
+  def create
+    if current_user
+      puts "review_params: #{review_params}"
+      @review = Review.new(review_params)
+
+      if @review.save
+        redirect_to product_url(params[:product_id]), notice: 'review created!'
+      else
+        redirect_to product_url(params[:product_id]), notice: 'some failure'
+      end
     end
   end
 
