16e1ba86a1826554827b54c31c0e9cfe114b94f6
RexChiu <rex.chiu@outlook.com>
2018-08-17 17:54:12 +0000

    added review controller code to accept the review submission form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,22 +1,24 @@
 class ReviewsController < ApplicationController
   def create
+    puts "review_params: #{review_params}"
     @review = Review.new(review_params)
 
     if @review.save
-      redirect_to products_url(@review.product_id), notice: 'review created!'
+      redirect_to product_url(params[:product_id]), notice: 'review created!'
     else
-      redirect_to products_url(@review.product_id), notice: 'some failure'
+      redirect_to product_url(params[:product_id]), notice: 'some failure'
     end
   end
 
   private
 
   def review_params
+    user_id = current_user ? current_user.id : -1
+    puts "User ID: #{user_id}"
+    puts "Current User: #{current_user}"
     params.require(:review).permit(
-      :product_id,
-      :user_id,
-      :dsecription,
-      :rating
-    )
+      :description,
+      :rating,
+    ).merge(product_id: params[:product_id], user_id: user_id)
   end
 end
