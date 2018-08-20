f5b35546175bdaf051f47fb3094d04b7b0d3282b
RexChiu <rex.chiu@outlook.com>
2018-08-20 16:14:25 +0000

    fixed bug of password_confirmation not being required

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+
   has_secure_password
 end
