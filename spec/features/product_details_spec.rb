04446271a691168778166e26afeed57f594525a7
Rex Chiu <rex.chiu@outlook.com>
2018-08-21 13:57:55 -0400

    added test for product details

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.feature 'ProductDetails', type: :feature, js: true do
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |_n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario 'They can click on a product' do
+    # ACT
+    visit root_path
+
+    puts @category.products.last.name
+    click_on @category.products.last.name
+
+    expect(find('.products-show')).not_to eq(nil)
+
+    # DEBUG / VERIFY
+    save_screenshot
+  end
+end
