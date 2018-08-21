bef38db1006ae08293ed7bc72cbddd55c0f2ab9a
Rex Chiu <rex.chiu@outlook.com>
2018-08-21 14:22:17 -0400

    added test for user login

---
+++ spec/features/user_logins_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.feature 'UserLogins', type: :feature, js: true do
+  before :each do
+    @user = User.create!(
+      first_name: 'Cats',
+      last_name: 'Cats',
+      email: 'cats@cats.cats',
+      password: 'Cats',
+      password_confirmation: 'Cats'
+    )
+  end
+
+  scenario 'They can login with valid credentials' do
+    # ACT
+    visit root_path
+
+    click_on 'Login'
+
+    expect(find('h1', text: 'Login')).not_to eq(nil)
+
+    fill_in 'email', with: 'cats@cats.cats'
+    fill_in 'password', with: 'Cats'
+    click_on 'Submit'
+
+    expect(find('h1', text: 'Products')).not_to eq(nil)
+
+    # DEBUG / VERIFY
+    save_screenshot
+  end
+end
